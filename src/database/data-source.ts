f2485fa775f981333f3796d3853b98a74d6e716d
hossam4444 <hosamtarek4444@gmail.com>
2025-04-12 09:36:43 +0200

    feat: add healthcheck configuration for API and define railway deployment settings

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,14 +6,31 @@ import { config } from 'dotenv';
 
 config();
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +(process.env.DB_PORT || 5432),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [User, Blog],
-  migrations: [SeedAdminAndBlogs1681171200000],
-  synchronize: false,
-});
+const getDatabaseConfig = () => {
+  if (process.env.DATABASE_URL) {
+    return {
+      url: process.env.DATABASE_URL,
+      type: 'postgres',
+      ssl: {
+        rejectUnauthorized: false
+      },
+      entities: [User, Blog],
+      migrations: [SeedAdminAndBlogs1681171200000],
+      synchronize: false,
+    };
+  }
+
+  return {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +(process.env.DB_PORT || 5432),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [User, Blog],
+    migrations: [SeedAdminAndBlogs1681171200000],
+    synchronize: false,
+  };
+};
+
+export const AppDataSource = new DataSource(getDatabaseConfig() as any);
